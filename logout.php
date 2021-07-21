e3db8bb8086049e27e1520239b2eb2ec5af79984
MikheilPapundze <55429620+MikheilPapundze@users.noreply.github.com>
2021-07-21 21:37:36 +0400

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-  //ახალი სესია იწყებაა როცა გამოვალთ სისტემიდან
-	session_start();
-	if(session_destroy()) {
-		//და დავბრუნდებით login-ის გვერდზე
-      header("Location: login.php");
-   }
-?>
